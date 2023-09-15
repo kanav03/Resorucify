78a3e3feb7d406abd57a45653058486657d8d5da
Harsh Patel <harshnpatel9@gmail.com>
2023-09-15 22:59:55 +0530

    chore: migrate to next

--- src/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import AuthRouter from './auth/auth.module';
-
-export default (): Router => {
-  const app = Router();
-
-  app.use('/auth', AuthRouter());
-  return app;
-};
